579fe39b047c2908b13ad4ccd35bf1f1f81f6300
baignoire57 <57708917+baignoire57@users.noreply.github.com>
2020-09-14 10:40:45 +0200

    Update Wp-cli-install.sh

--- Wp-cli-install.sh
+++ Wp-cli-install.sh
@@ -3,3 +3,6 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 php wp-cli.phar --info
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
+wget https://github.com/wp-cli/wp-cli/raw/master/utils/wp-completion.bash
+cat wp-completion.bash >> .bashrc
+source .bashrc
